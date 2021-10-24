811bfca3b725fc91b6e6c0da215521a66807704e
unknown <webdeveloperbashar>
2021-10-24 19:04:02 +0600

    again update

--- src/app/Routes.js
+++ src/app/Routes.js
@@ -2,7 +2,6 @@ import { Router } from "@reach/router";
 import Cart from "../pages/Cart";
 import Checkout from "../pages/Checkout";
 import Contact from "../pages/Contact";
-import Favorite from "../pages/Favorite";
 import ForgotPassword from "../pages/ForgotPassword";
 import Home from "../pages/Home";
 import Login from "../pages/Login";
@@ -33,7 +32,6 @@ const Routes = () => {
       <Cart path="/cart">
         <Cart />
       </Cart>
-      <Favorite path="/favourite" />
       <Checkout path="/checkout">
         <Checkout />
       </Checkout>
